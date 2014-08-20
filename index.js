d47fa1e51570c521bb0cf57d857bdee782591d19
THP <todderick@gmail.com>
2014-08-20 16:35:24 -0400

    Setup twitterytweets

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Learning happened via Contra's wearefractal/meatbot && Sam's omni5cience/meatspackles
 var config = require('./config.json');
 var io = require('socket.io-client');
+var twitter = require('./tweettweet');
 
 var Meatsorber = function() {
     if(config.apiKey === undefined) {
@@ -9,6 +10,7 @@ var Meatsorber = function() {
     this.address = config.address || 'https://chat.meatspac.es';
     this.apiKey = config.apiKey;
 
+    twitter('TEST');
     this.socket = io.connect(this.address);
     this.socket.on('message', function cb(data) {
 	var msg = data.chat.value;
---
+++ tweettweet.js
@@ -0,0 +1,25 @@
+var config = require('./config.json');
+var twAPI = require('node-twitter-api');
+
+var tweettweet = function(link) {
+    var tw = new twAPI({
+	consumerKey: config.twitterApiKey,
+	consumerSecret: config.twitterSecret,
+	callback: config.twitterCallback
+    });
+    tw.statuses("update", {
+	    status: "Meats Say" + link
+        },
+	config.twitterAccessToken,
+	config.twitterAccessSecret,
+	function(error, data, response) {
+	    if(error) {
+		console.log('ERROR: ' + error);
+	    } else {
+		console.log('Tweeted Successfully: ' + data);
+	    }
+	}
+    );
+}
+
+module.exports = tweettweet;
